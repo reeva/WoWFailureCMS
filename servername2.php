d088a5684ca3042998e28da68061f8b75adfd50e
FailZorD <alexfred@live.com>
2011-10-27 22:06:40 +0300

    Fixed some Errors ThankS: MRBillys
    Signed-off-by: FailZorD <alexfred@live.com>

--- servername2.php
+++ servername2.php
@@ -373,7 +373,7 @@ $bg = "<img src='wow/static/images/icons/faction/horde.gif' width='18' height='1
 echo '
 <tr>
 <td style="background-color: '.$bg.';"><b><center>'.$raw['name'].'</center></b></td>
-<td style="background-color: '.$bg.';"><center><img src="wow/static/images/icons/race/'.$raw[race].'-'.$raw[gender].'.gif" title="'.$race.' '.$gender.'"/></center></td>
+<td style="background-color: '.$bg.';"><center><img src="wow/static/images/icons/race/'.$raw['race'].'-'.$raw['gender'].'.gif" title="'.$race.' '.$gender.'"/></center></td>
 <td style="background-color: '.$bg.';"><center>'.$class.'</center></td>
 <td style="background-color: '.$bg.';"><center>'.$raw['level'].'</center></td>
 <td style="background-color: '.$bg.';"><center>'.$location.'</center></td>
