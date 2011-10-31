2583f74dfd962a22fb1b9e5b65b690ee936a5cf6
dju35 <dju35@hotmail.fr>
2011-10-31 12:36:33 -0500

    more work language

---
+++ lang/gr-gr.php
@@ -0,0 +1,80 @@
+<?php
+// Needs to be Completed.
+$lang['adb'] = "&Delta;&epsilon;&nu; &eta;&tau;&alpha;&nu; &delta;&upsilon;&nu;&alpha;&tau;&eta; &eta; &sigma;&upsilon;&nu;&delta;&epsilon;&sigma;&eta; &sigma;&tau;&eta;&nu; Account Database.";
+$lang['db'] = "&Delta;&epsilon;&nu; &eta;&tau;&alpha;&nu; &delta;&upsilon;&nu;&alpha;&tau;&eta; &eta; &sigma;&upsilon;&nu;&delta;&epsilon;&sigma;&eta; &sigma;&tau;&eta;&nu; Website Database.";
+$home['home'] = "&Alpha;&rho;&chi;&iota;&kappa;&eta;";
+$status['status'] = "&Kappa;&alpha;&tau;&alpha;&sigma;&tau;&alpha;&sigma;&eta;";
+$Community['Community'] = "&Kappa;&Omicron;&Iota;&Nu;&Omega;&Tau;&Eta;&Tau;&Alpha;";
+$Media['Media'] = "&Beta;&Iota;&Nu;&Tau;&Epsilon;&Omicron; &amp; &Mu;&Epsilon;&Sigma;&Alpha;";
+$Forums['Forums'] = "&Phi;&Omicron;&Rho;&omicron;&Upsilon;&Mu;";
+$Services['Services'] = "&Upsilon;&Pi;&Eta;&Rho;&Epsilon;&Sigma;&Iota;&Epsilon;&Sigma;";
+$Wow3['Wow3'] = " - &Epsilon;&Pi;&Iota;&Lambda;&Omicron;&Gamma;&Gamma;&eta; &Gamma;&Lambda;&Omega;&Sigma;&Sigma;&Omega;&Nu;";
+$Latest['Latest'] = "Latest Written Posts ";
+$View_all['View_all'] = "(&Pi;&rho;&omicron;&beta;&omicron;&lambda;&eta; &Omicron;&lambda;&omega;&nu;)";
+$all['all'] = "All Main Posts";
+$P_topics['P_topics'] = "&Delta;&eta;&mu;&omicron;&phi;&iota;&lambda;&eta; &Theta;&epsilon;&mu;&alpha;&tau;&alpha;";
+$click['click'] = " &Pi;&Alpha;&Tau;&Eta;&Sigma;&Tau;&Epsilon; ";
+$code_forum['code_forum']= " to view the &Phi;&Omicron;&Rho;&Upsilon;&Mu; &tau;&omicron;&nu; &Kappa;&omega;&delta;&iota;&kappa;&alpha;.";
+$here['here'] = "&epsilon;&delta;&omega;";
+$Account['Account'] = "&Lambda;&Omicron;&Gamma;&Alpha;&Rho;&Iota;&Alpha;&Sigma;&Mu;&Omicron;&Sigma;";
+$Account1['Account1'] = "&Delta;&epsilon;&nu; &mu;&pi;&omicron;&rho;&epsilon;&iota;&tau;&epsilon; &nu;&alpha; &sigma;&upsilon;&nu;&delta;&epsilon;&theta;&epsilon;&iota;&tau;&epsilon;;";
+$Account3['Account3'] = "&Delta;&eta;&mu;&iota;&omicron;&upsilon;&rho;&gamma;&iota;&alpha; &Lambda;&omicron;&gamma;&alpha;&rho;&iota;&alpha;&sigma;&mu;&omicron;&upsilon;";
+$Account4['Account4'] = "Account Summary";
+$Account5['Account5'] = "Account Security"; 
+$Account6['Account6'] = "Manage your Account";
+$Friends['Keepthem'] = "Keep your friends in game.";
+
+$Support['Support'] = "&Upsilon;&Pi;&Omicron;&Sigma;&Tau;&Eta;&Rho;&Iota;&Xi;&Eta;";
+$Support3['Support3'] = "Support Articles";
+$Support4['Support4'] = "Help! I got hacked!";
+$Support5['Support5'] = "Browse our support articles";
+$Support6['Support6'] = "Create a new support ticket";
+$Support7['Support7'] = "View your active support tickets.";
+$Support8['Support8'] = "&Kappa;&epsilon;&nu;&tau;&rho;&omicron; &Gamma;&nu;&omega;&sigma;&epsilon;&omega;&nu;";
+$Support9['Support9'] = "&Upsilon;&pi;&omicron;&beta;&alpha;&lambda;&epsilon;&tau;&epsilon; &mu;&iota;&alpha; &epsilon;&rho;&omega;&tau;&eta;&sigma;&eta;";
+$Support10['Support10'] = "Your Support Tickets";
+$Support11['Support11'] = "Get Support and explore the knowledgebase.";
+
+$Games['Games'] = "&Pi;&alpha;&iota;&chi;&nu;&iota;&delta;&iota;&alpha;";
+$Client_down['Client_down'] = "Game Client Downloads";
+$Client_down3['Client_down3'] = "Download World of Warcraft Client";
+
+
+$Americas['Americas'] = "Americas";
+$English['English'] = "English (US)";
+$Spanish['Spanish'] = "Espa&ntilde;ol (AL)";
+$Europe['Europe'] = "Europe";
+$Deutsch['Deutsch'] = "Deutsch";
+$Spanish3['Spanish3'] =  "Espa&ntilde;ol (EU)";
+$English3['English3'] = " English (EU)";
+$French['French'] = "Fran&ccedil;ais";
+$russia['russia'] = "Pycc&#1082;&#1080;&#1081;" ;
+$Korea3['Korea3'] = "&#54620;&#44397;&#51032;";
+$Korea['Korea'] = "Korea";
+$Taiwan3['Taiwan3'] = "&#32321;&#39636;&#20013;&#25991;";
+$Taiwan['Taiwan'] = "Taiwan";
+$China3['China3'] = "&#31616;&#20307;&#20013;&#25991;";
+$China['China'] = "China";
+$Greek['Greek'] = "Greek";
+$Greek['gr_lang'] = "&Epsilon;&lambda;&lambda;&eta;&nu;&iota;&kappa;&alpha; (EU)";
+$copyright['copyright'] = "Copyright &copy; 2011 ";
+$copyright3['copyright3'] = "Powered by WoWFailureCMS";
+$copyright4['copyright4'] = "Resources are in Blizzard Entertainment Property.";
+
+
+$Welcome['Welcome'] ="&Kappa;&alpha;&lambda;&omega;&sigmaf; &eta;&rho;&theta;&alpha;&tau;&epsilon;, ";
+$logout['logout'] = "&Alpha;&pi;&omicron;&sigma;&upsilon;&nu;&delta;&epsilon;&sigma;&eta;";
+$login['login'] = "&Sigma;&upsilon;&nu;&delta;&epsilon;&sigma;&eta;" ;
+
+$Donate1['Donate1'] = "Donate for"; 
+$Donate['Donate'] = "&Delta;&omega;&rho;&epsilon;&alpha;";
+
+$website['explore'] = "&Epsilon;&Xi;&Epsilon;&Rho;&Epsilon;&Upsilon;&Nu;&Eta;&Sigma;&Eta;";
+$More['More'] = "&Pi;&epsilon;&rho;&iota;&sigma;&sigma;&omicron;&tau;&epsilon;&rho;&alpha;";
+$Retrieve['Retrieve'] = "Retrieve Lost Account";
+$Find['Find'] = "Find us on the Map!";
+
+
+  
+
+?>
---
+++ lang/zh-cn.php
@@ -0,0 +1,80 @@
+<?php
+$lang['adb'] = "Could not connect to Account Database.";
+$lang['db'] = "Could not connect to Website Database.";
+$home['home'] = "home";
+$status['status'] = "Status";
+$Community['Community'] = "Community";
+$Media['Media'] = "Media";
+$Forums['Forums'] = "Forums";
+$Services['Services'] = "Services";
+$Wow['Wow'] = "World Of Warcraft ";
+$Wow3['Wow3'] = "World of Warcraft - Entire World";
+$Latest['Latest'] = "Latest Written Posts ";
+$View_all['View_all'] = "View all";
+$all['all'] = "All Main Posts";
+$P_topics['P_topics'] = "Popular Topics";
+$click['click'] = " Click ";
+$code_forum['code_forum']= " to view the Forums Code of Conduct.";
+$here['here'] = "here";
+$Account['Account'] = "Account";
+$Account1['Account1'] = "Can't log in?";
+$Account3['Account3'] = "Create Account";
+$Account4['Account4'] = "Account Summary";
+$Account5['Account5'] = "Account Security"; 
+$Account6['Account6'] = "Manage your Account";
+
+
+
+$Support['Support'] = "Support";
+$Support3['Support3'] = "Support Articles";
+$Support4['Support4'] = "Help! I got hacked!";
+$Support5['Support5'] = "Browse our support articles";
+$Support6['Support6'] = "Create a new support ticket";
+$Support7['Support7'] = "View your active support tickets.";
+$Support8['Support8'] = "Knowledge Center";
+$Support9['Support9'] = "Ask a Question";
+$Support10['Support10'] = "Your Support Tickets";
+$Support11['Support11'] = "Get Support and explore the knowledgebase.";
+
+$Games['Games'] = "Games";
+$Client_down['Client_down'] = "Game Client Downloads";
+$Client_down3['Client_down3'] = "Download World of Warcraft Client";
+
+$Americas['Americas'] = "Americas";
+$English['English'] = "English (US)";
+$Spanish['Spanish'] = "Espa&ntilde;ol (AL)";
+$Europe['Europe'] = "Europe";
+$Deutsch['Deutsch'] = "Deutsch";
+$Spanish3['Spanish3'] =  "Espa&ntilde;ol (EU)";
+$English3['English3'] = " English (EU)";
+$French['French'] = "Fran&ccedil;ais";
+$russia['russia'] = "Pycc&#1082;&#1080;&#1081;" ;
+$Korea3['Korea3'] = "&#54620;&#44397;&#51032;";
+$Korea['Korea'] = "Korea";
+$Taiwan3['Taiwan3'] = "&#32321;&#39636;&#20013;&#25991;";
+$Taiwan['Taiwan'] = "Taiwan";
+$China3['China3'] = "&#31616;&#20307;&#20013;&#25991;";
+$China['China'] = "China";
+$Greek['Greek'] = "Hellenic";
+$Greek['gr_lang'] = "&Epsilon;&lambda;&lambda;&eta;&nu;&iota;&kappa;&alpha; (EU)";
+$copyright['copyright'] = "Copyright &copy; 2011 ";
+$copyright3['copyright3'] = "Powered by WoWFailureCMS";
+$copyright4['copyright4'] = "Resources are in Blizzard Entertainment Property.";
+
+
+$Welcome['Welcome'] ="Welcome, ";
+$logout['logout'] = "Log Out";
+$login['login'] = "Log In" ;
+
+$Donate1['Donate1'] = "Donate for"; 
+$Donate['Donate'] = "Donate";
+
+$website['explore'] = "Explore";
+$More['More'] = "More";
+$Retrieve['Retrieve'] = "Retrieve Lost Account";
+$Find['Find'] = "Find us on the Map!";
+
+
+  
+
+?>
