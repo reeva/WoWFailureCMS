302cc4136d8057a1d645e96e9a44865830a9c38b
FailZorD <alexfred@live.com>
2011-11-15 22:28:17 +0200

    Clean up
    Signed-off-by: FailZorD <alexfred@live.com>

--- forum/configs.php
+++ forum/configs.php
@@ -1,47 +1,43 @@
 <?php
-session_start();
 session_save_path('../phpsessions');
-
+session_start();
 
 /*||||||||||||||||||||||||||||||||||||||||*/
 /*|||||||The Language Configuration|||||||*/
 /*|||||||||||DO NOT TOUCH THIS||||||||||||*/
 /*||||||||||||||||||||||||||||||||||||||||*/
-if(!empty($_GET['Local']) || !empty($_SESSION['Local'])) {
+$lang = &$_SESSION['Local'];
 $lang = $_GET['Local'];
-$_SESSION['Local'] = $lang;
-$lang = $_SESSION['Local'];
-}
-else {
-$lang = substr($_SERVER["HTTP_ACCEPT_LANGUAGE"], 0,2);
-}
 switch ($lang)
 {
-    case 'us' :
+    case 'en-us' :
         include ("../lang/en-us.php");
         break;
 	case 'ro-ro' :
         include ("../lang/ro-ro.php");
         break;
-    case 'en' :
+    case 'en-gb' :
         include ("../lang/en-gb.php");
         break;
-    case 'de' :
+	case 'it-it' :
+        include ("../lang/it-it.php");
+        break;
+    case 'de-de' :
         include ("../lang/de-de.php");
         break;
     case 'bu-bu' :
         include ("../lang/bg-bg.php");
         break;
-    case 'es' :
+    case 'es-es' :
         include ("../lang/es-es.php");
         break;
     case 'es-mx' :
         include ("../lang/es-mx.php");
         break;
-    case 'gr' :
+    case 'gr-gr' :
         include ("../lang/gr-gr.php");
         break;
-    case 'ru' :
+    case 'ru-ru' :
         include ("../lang/ru-ru.php");
         break;
     case 'zh-cn' :
@@ -50,12 +46,9 @@ switch ($lang)
     case 'zh-tw' :
         include ("../lang/zh-tw.php");
         break;
-    case 'fr' :
+    case 'fr-fr' :
         include ("../lang/fr-fr.php");
-        break;
-    case 'it' :
-        include ("../lang/it-it.php");
-        break; 		
+        break;         
     default :
         include ("../lang/en-us.php");
         $lang = 'en-us';
@@ -64,8 +57,8 @@ switch ($lang)
 /*||||||||The Server Configuration||||||||*/
 /*||||||||||||||||||||||||||||||||||||||||*/
 $serveraddress = "localhost"; // Your MySQL server address
-$serveruser = "billys07_7"; // Your MySQL user
-$serverpass = "mario123"; //Your MySQL password
+$serveruser = "root"; // Your MySQL user
+$serverpass = "ascent"; //Your MySQL password
 $serverport	= "3306"; // Your MySQL Port
 $donatadmin = "ascent"; // Your Donation Admin Password
 $website['realm'] = "Set Realmlist Your_Realmlist"; // You realm (Example: Set RealmList login.wowfailure.com)
@@ -76,20 +69,20 @@ $website['realm'] = "Set Realmlist Your_Realmlist"; // You realm (Example: Set R
 /*||||||||||||||||||||||||||||||||||||||||*/
 /*|||||||The Website Configuration||||||||*/
 /*||||||||||||||||||||||||||||||||||||||||*/
-$server_db = "billys07_failure"; //Your website database
-$server_adb = "Ezalion-Auth"; //Your account database
-$server_cdb = "Ezalion-Characters"; //Your characters database
-$server_cdb_2 = "Ezalion-Characters"; //Your characters database
-$server_wdb = "Ezalion-World"; //Your world database
-$server_wdb_2 = "Ezalion-World"; //Your world database
+$server_db = "site"; //Your website database
+$server_adb = "auth"; //Your account database
+$server_cdb = "characters"; //Your characters database
+$server_cdb_2 = "characters2"; //Your characters database
+$server_wdb = "world"; //Your world database
+$server_wdb_2 = "world"; //Your world database
 $charTable = 'characters'; //Your Character Table!!
 $name_realm1['realm'] = "Server_Name_1"; //Your Realm Name 1
 $name_realm2['realm'] = "Server_Name_2"; //Your Realm Name 2
 $charLimit = '10'; //Max characters to show on the page
 $website['title'] = "WoWFailureCMS"; // Your Website Title
 $website['slogan'] = "WoWFailureCMS, get your best CMS today, simple and fast!"; // Your Website Slogan.
-$website['address'] = "http://www.wowfailure.co.cc"; // 'http://url/foldername/' or 'http://url/'
-$website['root'] = "./"; // '/' <- for root and '/foldername/' <- for any folder
+$website['address'] = "http://www.wowfailure.com"; // 'http://url/foldername/' or 'http://url/'
+$website['root'] = "/WoWFailureCMS/"; // '/' <- for root and '/foldername/' <- for any folder
 /*||||||||||||||||||||||||||||||||||||||||*/
 /*||||||||||||DO NOT EDIT BELOW|||||||||||*/
 /*||||||||||||||||||||||||||||||||||||||||*/
