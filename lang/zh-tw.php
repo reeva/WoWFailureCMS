f43a11e8e9df4411d7fed456266a99edd3d85af7
MrBillys <kallitheotis04@gmail.com>
2011-11-19 01:55:08 +0200

    added more traslation...
    Signed-off-by: MrBillys <kallitheotis04@gmail.com>

--- lang/zh-tw.php
+++ lang/zh-tw.php
@@ -80,6 +80,8 @@ $website['explore'] = "Explore";
 $More['More'] = "More";
 $Retrieve['Retrieve'] = "Retrieve Lost Account";
 $Find['Find'] = "Find us on the Map!";
+  
+// Starting the voting translation
 
 $Vote['Vote'] = "Sistema de Votación";
 $Vote['Vote1'] = "Sistema de Votación de ";
@@ -99,6 +101,182 @@ $Vote['Vote13'] = "Hora";
 $Vote['Vote14'] = "Cantidad de Puntos Gastados";
 $Vote['Vote15'] = "Cantidad de Puntos Ganados";
 $Vote['Vote16'] = "Orden";
-  
 
+// Finishing the voting translation
+// Starting register
+$Reg['Reg'] = "Required";
+$Reg['Reg1'] = "Account Settings";
+$Reg['Reg2'] = "Change Your Password";
+$Reg['Reg3'] = "You ";
+$Reg['Reg4'] = "MUST";
+$Reg['Reg5'] = " be offline for this tool to successfully work! Plus you need to be Loged to the website. Use this form to change your password.";
+$Reg['Reg6'] = "Error:";
+$Reg['Reg7'] = "New password fields must match!";
+$Reg['Reg8'] = "Please go back and try again.";
+$Reg['Reg9'] = "Your new password is too short!";
+$Reg['Reg10'] = "You entered";
+$Reg['Reg11'] = "character(s).";
+$Reg['Reg12'] = "The minimum length is 5 characters and the maximum length is 15.";
+$Reg['Reg13'] = "Please go back and try again.";
+$Reg['Reg14'] = "Invalid account name/password!";
+$Reg['Reg15'] = "Please go back and try again.";
+$Reg['Reg16'] = "Password for the Account";
+$Reg['Reg17'] = "has been successfully changed!";
+$Reg['Reg18'] = "Account Name:";
+$Reg['Reg19'] = "Old Password:";
+$Reg['Reg20'] = "New Password:";
+$Reg['Reg21'] = "Password Rules";
+$Reg['Reg22'] = "Your password may only contain ";
+$Reg['Reg23'] = "alphabetic characters (A–Z), numeric characters (0–9), and punctuation.";
+$Reg['Reg24'] = "Your password ";
+$Reg['Reg25'] = "must";
+$Reg['Reg26'] = " contain at least one alphabetic character ";
+$Reg['Reg27'] = "and";
+$Reg['Reg28'] = " one numeric character.";
+$Reg['Reg29'] = "You cannot enter your account name as your password.";
+$Reg['Reg30'] = "Your password must be between ";
+$Reg['Reg31'] = "eight and sixteen characters";
+$Reg['Reg32'] = " in length.";
+$Reg['Reg33'] = "For your security, we highly recommend you choose a unique password that you don’t use for any other online account.";
+$Reg['Reg34'] = "Confirm New Password:";
+$Reg['Reg35'] = "Continue";
+$Reg['Reg36'] = "Cancel ";
+$Reg['Reg37'] = " - Change Password";
+// Finishing register
+// Starting Account Log
+$Log['Log'] = " - Login";
+$Log['Log1'] = "Battle.net";
+$Log['Log2'] = "Log In";
+$Log['Log3'] = "Logging In";
+$Log['Log4'] = "Wrong Password";
+$Log['Log5'] = "Invalid Account Name";
+$Log['Log6'] = "Account Name";
+$Log['Log7'] = "Password";
+$Log['Log8'] = "Keep me logged in";
+$Log['Log9'] = "Log In";
+$Log['Log10'] = "You're Logged In";
+$Log['Log11'] = "Can't log in";
+$Log['Log12'] = "Don't have an account yet? ";
+$Log['Log13'] = "Sign up now";
+$Log['Log14'] = "Learn how to ";
+$Log['Log15'] = "protect your account";
+$Log['Log16'] = "Already Logged in?";
+$Log['Log17'] = "If you are already logged but ths page does not redirect you to the account then press the following button: ";
+$Log['Log18'] = "Account Management";
+// Finishing Account Log
+// Starting Acccount Man
+$Man['Man1'] = " - Account Management";
+$Man['Man2'] = "JavaScript must be enabled to use this site.";
+$Man['Man3'] = "Game Management";
+$Man['Man4'] = "World of Warcraft&#174;: Cataclysm";
+$Man['Man5'] = "Account Name";
+$Man['Man6'] = "Status";
+$Man['Man7'] = "Active";
+$Man['Man8'] = "Banned";
+$Man['Man9'] = "Current Game Source";
+$Man['Man10'] = "100% Days";
+$Man['Man11'] = "Expansion Pack";
+$Man['Man12'] = "Cataclysm";
+$Man['Man13'] = "Change";
+$Man['Man14'] = "Wrath of the Lich King";
+$Man['Man15'] = "Change";
+$Man['Man16'] = "The Burning Crusade";
+$Man['Man17'] = "Change";
+$Man['Man18'] = "World of Warcraft";
+$Man['Man19'] = "Change";
+$Man['Man20'] = "Region";
+$Man['Man21'] = "Europe (EU)";
+$Man['Man22'] = " Donator";
+$Man['Man23'] = "Unsubscribed";
+$Man['Man24'] = "Donate / Buy Subscription";
+$Man['Man25'] = "Vote and Get Rewarded";
+$Man['Man26'] = "Change your Password";
+$Man['Man27'] = "View Account History";
+$Man['Man28'] = "Download Beginner's Guide";
+$Man['Man29'] = "Download Game Client";
+$Man['Man30'] = "Enter your Game Key";
+$Man['Man31'] = "Upgrade Account";
+$Man['Man32'] = "Cancel ";
+$Man['Man33'] = "Not case-sensitive, no spaces or hyphens required.";
+$Man['Man34'] = "Character Services";
+$Man['Man35'] = "Additional Services";
+$Man['Man36'] = "Referrals &amp; Rewards";
+$Man['Man37'] = "Game Time &amp; Subscriptions";
+$Man['Man38'] = "Character Transfer";
+$Man['Man39'] = "Move your characters to different realms or accounts.";
+$Man['Man40'] = "Faction Change";
+$Man['Man41'] = "Change a character's faction (Horde to Alliance or Alliance to Horde).";
+$Man['Man42'] = "Race Change";
+$Man['Man43'] = "Change a character's race (within your current faction).";
+$Man['Man44'] = "Name Change";
+$Man['Man45'] = "Change your characters' names.";
+$Man['Man46'] = "Appearance Change";
+$Man['Man47'] = "Change your characters' appearance (Optional name change included).";
+$Man['Man48'] = "Character Unstuck";
+$Man['Man49'] = "Transfer your characters when he gets stuck.";
+$Man['Man50'] = "Vote Shop";
+$Man['Man51'] = "Vote and get rewarded and support ";
+$Man['Man52'] = "to be on the Top List.";
+$Man['Man53'] = "Beta Test Realm (Closed)";
+$Man['Man54'] = "Arena Pass registration is currently closed. Click here for more information.";
+$Man['Man55'] = "Parental Controls";
+$Man['Man56'] = "Manage, monitor, and limit your child's play time.";
+$Man['Man57'] = "Recruit a Friend";
+$Man['Man58'] = "Earn in-game rewards, and more by recruiting your friends, bringing them to the Ultimate Gaming.";
+$Man['Man59'] = "Refer a Friend";
+$Man['Man60'] = "Get more Friends, to ";
+$Man['Man61'] = ", to enjoy the Ultimate Gaming.";
+$Man['Man62'] = "Donate Subscription";
+$Man['Man63'] = "Add ";
+$Man['Man64'] = " Donation Title to your account and unlock illuminative Rewards.";
+$Man['Man65'] = " Remote";
+$Man['Man66'] = "Use all the features of the Remote Auction House of ";
+// Finishing Account Man
+//Starting Ban List
+$BanL['BanL1'] = "Banned By:";
+$BanL['BanL2'] = "Unban Date:";
+$BanL['BanL3'] = "Duration:";
+$BanL['BanL4'] = "Reason";
+$BanL['BanL5'] = "Permanent";
+$BanL['BanL6'] = "Never";
+// Finishing Ban List
+//Starting Chatbox
+$ChatB['ChatB1'] = "Shout was empty.";
+$ChatB['ChatB2'] = "You Must Login To Shout.";
+$ChatB['ChatB3'] = "Page:";
+$ChatB['ChatB4'] = "Previous";
+$ChatB['ChatB5'] = "Next";
+//Finishing Chatbox
+// Starting the Char Unstuck translation
+$Uns['Uns'] = " - Character Unstuck";
+$Uns['Uns1'] = "Progress 100%";
+$Uns['Uns2'] = " [Step 1 of 1]";
+$Uns['Uns3'] = "Game Region Warning";
+$Uns['Uns4'] = "You should change your country to Korea, republic of and verify your Personal Residence Number to use Korean games.";
+$Uns['Uns5'] = "Game Region Warning";
+$Uns['Uns6'] = "The World of Warcraft account merge process is not yet available in your region.";
+$Uns['Uns7'] = "If you lost your authenticator, please detach it before proceeding. (Security card service ended 8/13/2010 and was automatically detached.) ";
+$Uns['Uns8'] = "If you use Phone Lock: please unlock your World of Warcraft account before merging.";
+$Uns['Uns9'] = "Taiwan: 0800-303-585";
+$Uns['Uns10'] = "(Not available 10–11AM every first Wed of the month)";
+$Uns['Uns11'] = "Hong Kong &amp; Macau: 396-54666";
+$Uns['Uns12'] = " Required";
+$Uns['Uns13'] = "Game Management";
+$Uns['Uns14'] = "Unstuck your World of Warcraft Character";
+$Uns['Uns15'] = "Enter the username and password of the World of Warcraft account and the Character you wish to transfer from the stucked area. You will have the opportunity to move or to continue your journey ingame. You must be offline from your account to let this succeed.";
+$Uns['Uns16'] = "World of Warcraft Account Name:";
+$Uns['Uns17'] = "World of Warcraft Account Character:";
+$Uns['Uns18'] = "World of Warcraft Account Password:";
+$Uns['Uns19'] = "Forgot your password?";
+$Uns['Uns20'] = "If you have forgotten your World of Warcraft password, please contact ";
+$Uns['Uns21'] = "Customer Support";
+$Uns['Uns22'] = "Unstuck!";
+$Uns['Uns23'] = "Cancel ";
+$Uns['Uns24'] = "Read more";
+$Uns['Uns25'] = "Invalid Account.";
+$Uns['Uns26'] = "Invalid Character.";
+$Uns['Uns27'] = "That Character Is Not Yours.";
+$Uns['Uns28'] = "Your Character Is Fixed.";
+$Uns['Uns29'] = "Continue";
+// Finishing the Char Unstuck translation
 ?>
