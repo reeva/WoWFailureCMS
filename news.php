e43b5fd735c3fda1f924370a3809120c10dc0e7f
FailZorD <alexfred@live.com>
2011-11-27 23:17:54 +0200

    Updated the news and finished the 4.3.0 SQL Update for news, use this only if you support 4.3.0 Private Server.
    Signed-off-by: FailZorD <alexfred@live.com>

--- news.php
+++ news.php
@@ -50,6 +50,14 @@ _gaq.push(['_setDomainName', '.battle.net']);
 _gaq.push(['_trackPageview']);
 //]]>
 </script>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 </head>
 <body class="en-gb logged-in">
 <div id="wrapper">
@@ -171,11 +179,7 @@ _gaq.push(['_trackPageview']);
 												<a class="addthis_button_digg at300b" href="http://www.addthis.com/bookmark.php?v=250&amp;winname=addthis&amp;pub=blizzardwebteam&amp;source=tbx-250&amp;lng=en-US&amp;s=digg&amp;url=http%3A%2F%2Feu.battle.net%2Fwow%2Fen%2Fblog%2F2192483%23blog&amp;title=Memories%20of%20Blizzard%20Video%20Contest%20Entries%20Closed%20-%20World%20of%20Warcraft&amp;ate=AT-blizzardwebteam/-/-/4daf164f55138edd/4&amp;uid=4daf164f14677003&amp;CXNID=2000001.5215456080540439074NXC&amp;pre=http%3A%2F%2Feu.battle.net%2Fwow%2Fen%2F&amp;tt=0" target="_blank" title="Digg This"><span class="at300bs at15nc at15t_digg"></span></a>
 											<div class="atclear"></div></div>-->
 											<!-- ADDTHIS BUTTON END -->
-											<script type="text/javascript">
-											Core.appendFrame(\'http://www.facebook.com/plugins/like.php?href=http://theadriann.zapto.org/TrinityCMS/&amp;layout=button_count&amp;show_faces=false&amp;width=450&amp;action=like&amp;font=lucida+grande&amp;colorscheme=light&amp;height=21\', 120, 21, $(\'.community-share\'));
-											//<![CDATA[
-											//]]>
-											</script>
+											<div class="fb-like" data-href="http://www.facebook.com/pages/WoWFailureCMS/141791002519526?ref=ts" data-send="true" data-layout="button_count" data-width="450" data-show-faces="false"></div>
 										</div>';
 										$show_comment=true;
 										}
