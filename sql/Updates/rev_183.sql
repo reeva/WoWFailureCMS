bc9549040d651c38a779e4b9e1996466c7b20e1c
FailZorD <alexfred@live.com>
2012-03-01 16:52:05 +0200

    Some Small Corrections
    Signed-off-by: FailZorD <alexfred@live.com>

--- sql/Updates/rev_183.sql
+++ sql/Updates/rev_183.sql
@@ -1,6 +1,6 @@
 /*Just if your characters db name is characters*/
-UPDATE website.realms SET char_db = 'characters' WHERE id>0; 
+UPDATE realms SET char_db = 'characters' WHERE id>0; 
 
-ALTER TABLE website.realms MODIFY char_db VARCHAR(255) NOT NULL DEFAULT 'characters';
+ALTER TABLE realms MODIFY char_db VARCHAR(255) NOT NULL DEFAULT 'characters';
 
-UPDATE website.version SET Revision='183' WHERE Name='AquaFlameCMS';
+UPDATE `version` SET `Revision`='183' WHERE `Name`='AquaFlameCMS';
