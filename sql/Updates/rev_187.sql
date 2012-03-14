de6a57d34fe6e604afbdd45b5df9b25524115f2b
Msancho <msanchogin@gmail.com>
2012-03-15 00:36:12 +0100

    Update table for new media system

---
+++ sql/Updates/rev_187.sql
@@ -0,0 +1,5 @@
+﻿ALTER TABLE `video_comments` RENAME TO `media_comments`,
+  CHANGE COLUMN `videoid` `mediaid` INT(10) NOT NULL,
+  MODIFY COLUMN `accountId` INT(10) NOT NULL;
+
+UPDATE `version` SET `Revision`='187' WHERE `Name`='AquaFlameCMS';
